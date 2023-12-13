c8927077427d7df67a1ee8db559901c67a6e97d9
eniyakrishnaraj <78894087+eniyakrishnaraj@users.noreply.github.com>
2023-12-13 02:42:10 -0800

    Delete models directory

--- models/genetic_sequence.py
+++
@@ -1,4 +0,0 @@
-@dataclass(frozen=True)
-class GeneticSequence:
-  sequence: str
-  is_dna: bool  # Flag to distinguish DNA (True) or RNAa (False)
--- models/protein.py
+++
@@ -1,3 +0,0 @@
-@dataclass(frozen=True)
-class Protein:
-  sequence: str

05076dd7c7c4e6c0b8d46ebe286cfb1e390fbda8
eniyakrishnaraj <78894087+eniyakrishnaraj@users.noreply.github.com>
2023-12-13 04:57:56 -0800

    Initial commit

--- tests/test_translate.py
+++ tests/test_translate.py
@@ -15,17 +15,17 @@ class TestTranslate(unittest.TestCase):
     # Test an invalid DNA sequence (not divisible by 3)
     invalid_dna_sequence = "ATGT"
     result = self.translator.run(invalid_dna_sequence)
-    self.assertEqual(result, "")
+    self.assertEqual(result, "Dna sequence is not valid.") 
 
     # Test an empty DNA sequence
     empty_dna_sequence = ""
     result_empty = self.translator.run(empty_dna_sequence)
-    self.assertEqual(result_empty, "")
+    self.assertEqual(result_empty, "Dna sequence is not valid.")
 
   def test_invalid_nucleotide(self):  # Test a DNAA sequence with an invalid nucleotide
     invalid_nucleotide_sequence = "ATGTXATCGTAG"
     result_invalid_nucleotide = self.translator.run(invalid_nucleotide_sequence)
-    self.assertEqual(result_invalid_nucleotide, "")
+    self.assertEqual(result_invalid_nucleotide, "Invalid nucleotide found in DNA sequence."
 
 if __name__ == "__main__":
   unittest.main()

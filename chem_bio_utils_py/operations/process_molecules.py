3ce83ac849d8c7c1f486b816da7b388ce9058783
eniyakrishnaraj <78894087+eniyakrishnaraj@users.noreply.github.com>
2023-12-15 07:26:49 -0800

    Create process_molecules.py

---
+++ chem_bio_utils_py/operations/process_molecules.py
@@ -0,0 +1,14 @@
+from chem_bio_utils_py.models.molecule import MoleculeModel
+
+def processMolecule(input):  # Can take in either SMILES or InChI
+  # Check that input is not empty string
+  if not input:
+    return "Input is empty."
+
+  # Processes the input and returns a molecule with all the attributes
+  try:
+    molecule = MoleculeModel(input)
+    return molecule
+  except ValueError as e:
+    print(f"Error processing input '{input}]: {e}")
+    return None

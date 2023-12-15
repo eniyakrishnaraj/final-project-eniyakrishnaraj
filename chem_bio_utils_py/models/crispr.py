0a934f8939582ebbdd88d4c63e9f1ce7fa7ab557
eniyakrishnaraj <78894087+eniyakrishnaraj@users.noreply.github.com>
2023-12-14 23:15:34 -0800

    Update crispr.py

--- chem_bio_utils_py/models/crispr.py
+++ chem_bio_utils_py/models/crispr.py
@@ -3,4 +3,10 @@ from dataclasses import dataclass
 @dataclass(frozen=True)
 class crispr:
   name: str
-  pam_sequence: str
+  pam_sequence: str  # Specify the PAM sequence of the CRISPR system
+  target_type: str  # Specify if the target is DNA or RNA
+  spacer_length: int  # Spacer length for the system
+  for_prefix: str  # Region necessary for Cas binding or reecognition
+  for_suffix: str  # Region necessaary for increased stability
+  rev_prefix: str  # Prefix for reverse oligo
+  rev_suffix: str  # Suffix for reverse oligo

1e5c38da423681f46201bac110374a0fddd2e3c7
eniyakrishnaraj <78894087+eniyakrishnaraj@users.noreply.github.com>
2023-12-15 08:25:07 -0800

    Update test_process_molecule.py

--- tests/test_process_molecule.py
+++ tests/test_process_molecule.py
@@ -2,17 +2,13 @@ import unittest
 from chem_bio_utils_py.operations.process_molecules import processMolecule
 
 class TestProcessMolecule(unittest.TestCase):
-  def setUp(self):
-    smiles = "[H-]"
-    inchi = "InChI=1S/H/q-1"
-    
   def test_valid_smiles(self):  # Test a valid SMILES string
-    smiles = "[H-]"
-    inchi = "InChI=1S/H/q-1"
-    result = processMolecule(smiles)
+    input = "[H-]"
+    result_inchi = "InChI=1S/H/q-1"
+    result = processMolecule(input)
     self.assertIsNotNone(result)
     self.assertNotIsInstance(result, str)
-    self.assertEqual(inchi, result.inchi)
+    self.assertEqual(result_inchi, result.inchi)
 
   def test_invalid_smiles(self):  # Test an invalid SMILES string
     invalid_smiles = "InvalidSMILES"
@@ -21,12 +17,12 @@ class TestProcessMolecule(unittest.TestCase):
     self.assertEqual(result, "Error processing input 'InvalidSMILES': Invalid input. Not SMILES or InChI.")
 
   def test_valid_inchi(self):  # Test a valid InChI string
-    inchi = "InChI=1S/H/q-1"
-    smiles = "[H-]"
-    result = processMolecule(inchi)
+    input = "InChI=1S/H/q-1"
+    result_smiles = "[H-]"
+    result = processMolecule(input)
     self.assertIsNotNone(result)
     self.assertNotIsInstance(result, str)
-    self.assertEqual(smiles, result.smiles)
+    self.assertEqual(result_smilese, result.smiles)
 
   def test_invalid_inchi(self):  # Test an invalid InChI string
     invalid_inchi = "InvalidInChI"

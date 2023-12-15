b2a94ed7a8a2ff9e73f3102e78abe445a7072b34
eniyakrishnaraj <78894087+eniyakrishnaraj@users.noreply.github.com>
2023-12-14 18:34:30 -0800

    Update test_transcription.py

--- tests/test_transcription.py
+++ tests/test_transcription.py
@@ -7,13 +7,13 @@ class TestDNATranscriber(unittest.TestCase):
 
   def test_transcribe(self):  # Tests a valid DNA seequence
     dna_sequence = "ATGTCATTCGTAG"
-    expected_rna_sequence = "UACAGUAGCUA"
+    expected_rna_sequence = "UACAGUAAGCAUC"
     result = self.transcriber.transcribe(dna_sequence)
     self.assertEqual(result, expected_rna_sequence)
 
   def test_lowercase_sequence(self):  # Test a DNA sequence with lowercase letters
     lowercase_dna_sequence = "atgtcattcgtag"
-    expected_rna_sequence = "UACAGUAGCUA"
+    expected_rna_sequence = "UACAGUAAGCAUC"
     result_lowercase = self.transcriber.transcribe(lowercase_dna_sequence)
     self.assertEqual(result_lowercase, expected_rna_sequence)
     

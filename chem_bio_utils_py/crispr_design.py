e559d93cfdeb404bf7536d5233a489c4a6894b78
eniyakrishnaraj <78894087+eniyakrishnaraj@users.noreply.github.com>
2023-12-15 02:26:25 -0800

    Update crispr_design.py

--- chem_bio_utils_py/crispr_design.py
+++ chem_bio_utils_py/crispr_design.py
@@ -1,31 +1,56 @@
 from chem_bio_utils_py.models import CRISPR
+from chem_bio_utils_py.reverse_complement import ReverseComplement
 
 class CRISPRDesign:
-  def __init__(self, target_sequence, organism_genome):
-    self.target_sequence = target_sequence
-    self.organism_genome = organism_genome
+  def __init__(self):
     self.valid_crispr_systems = self.initialize.crispr_systems()
+    self.valid_nucleotides = set("ATCGU")
+    self.revcomp = ReverseComplement()
 
+  """This method is meant to initialize the CRISPR systtems. All the attributes were designed based
+     on literature review (sources below). This initialization can obviously be added upon as 
+     more research is conducted on more CRISPR systems"""
   def initialize_crispr_systems(self):
-    cas9 = CRISPR(name='Cas9', pam_sequence='NGG')  # Where N can be any nucleotide
-    cas12a = CRISPR(name='Cas12a', pam_sequencee='TTTV')  # Where V can be 'A', 'C', 'G'
-    cas12b = CRISPR(name='Cas12b', pam_sequencee='TCCC')
-    cas12c = CRISPR(name='Cas12c', pam_sequencee='TTN')
-    cas13a = CRISPR(name='Cas13a', pam_sequencee='TTTV')
-    cas13b = CRISPR(name='Cas13b', pam_sequencee='TTC')
-    cas14 = CRISPR(name='Cas14', pam_sequencee='TAA')
-    c2c1 = CRISPR(name='C2c1', pam_sequencee='TTCN')
-    c2c2 = CRISPR(name='C2c2', pam_sequencee='TTN')
-    c2c3 = CRISPR(name='C2c3', pam_sequencee='TTN')
-    c2c6 = CRISPR(name='C2c6', pam_sequencee='TTTN')
-    c2c7 = CRISPR(name='C2c7', pam_sequencee='TTTN')
-    c2c10 = CRISPR(name='C2c10', pam_sequencee='TTN')
+    # Simple, efficient and open-source CRISPR/Cas9 strategy for multi-site genome editing in Populus tremula × alba: https://academic.oup.com/treephys/article/41/11/2216/6270869
+    cas9 = CRISPR(name='Cas9', pam_sequence='NGG', spacer_length=20, for_prefix='AATGGTCTCAA', for_suffix='GCTTAGAGACCAAT', rev_prefix='CTCACTAGT', rev_suffix='TAG')
+    # Repeat expansion and methylation state analysis with nanopore sequencing: https://www.biorxiv.org/content/10.1101/480285v1.full
+    cas12a = CRISPR(name='Cas12a', pam_sequence='TTTV', spacer_length=30, for_prefix='CGGCAGCC', for_suffix='GGAAAAAC', rev_prefix='TTAGCGGCTT', rev_suffix='GGCAAAAGAG')
+    # Engineering of CRISPR-Cas12b for human genome editing: https://www.nature.com/articles/s41467-018-08224-4#MOESM1
+    cas12b = CRISPR(name='Cas12b', pam_sequence='TCCC', spacer_length=30, for_prefix='ATTNT', for_suffix='CTT', rev_prefix='GAGCTGCG', rev_suffix='CTGTGATAC')
+    # A naturally DNase-free CRISPR-Cas12c enzyme silences gene expression: https://www.sciencedirect.com/science/article/pii/S1097276522003835#mmc3
+    cas12c = CRISPR(name='Cas12c', pam_sequence='TTN', spacer_length=30, for_prefix='GCCTGCCCGCAGA', for_suffix='CTTTCCAGTC', rev_prefix='TGGCTGT', rev_suffix='ACCGTAA')
 
-    return [cas9, cas12a, cas12b, cas12c, cas13a, cas13b, cas14, c2c1, c2c2, c2c3, c2c6, c2c7, c2c10]
+    return [cas9, cas12a, cas12b, cas12c]
 
-  def run(self, crispr):
-    # Check if the specified CRISPR system is valid
-    if crispr not in self.valid_crispr_systems:
-      return "Not a valid CRISPR system at this time."
+  def designOligos(self, cds, crispr_system_name):
+    # Find the CRISPR system by name
+    crispr = next((c for c in self.valid_crispr_systems if c.name == crispr_system_name), None)
+    if not crispr:
+      return f"CRISPR system '{crispr_system_name}' not found or not supported. Only cas9, cas12a, cas12b, and caas12c are supported."
+      
+    # Check that cds is not an empty string
+    if not cds:
+      return "CDS is empty."
 
-    
+    # Check if the length of the cds is long enough based on the spacer length and PAM sequence
+    if len(cds) < (crispr.spacer_length + len(crispr.pam_sequence)):
+      return "CDS provided is too short to contain a target site"
+
+    # Check that the cds only has appropriate nucleotides
+    cds = cds.upper()
+    if not all(nucleotide in self.valid_nucleotides for nucleotide in cds):
+      return "CDS is not DNA or RNA. Invalid nucleotide found."
+
+    # Calculate the start index for the PAM sequence
+    PAM_start = cds.find(crispr.pam_sequence, crispr.spacer_length + len(crispr.pam_sequence)) - 1
+    if PAM_start < 0:
+      return "CDS contains no target site on this strand"
+
+    # Construct the spacer
+    spacer = cds[PAM_start - crispr.spacer_length:PAM_start]
+    revCompSpacer = revcomp.reverseComp(spacer)
+
+    for_oligo = crispr.for_prefix + spacer + crispr.for_suffix
+    rev_oligo = crispr.rev_prefix + revCompSpacer + crispr.rev_suffix
+
+    return for_oligo, rev_oligo

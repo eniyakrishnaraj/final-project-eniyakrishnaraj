106c9bec00396a692c2f32af3f5fabbdb763c9e0
eniyakrishnaraj <78894087+eniyakrishnaraj@users.noreply.github.com>
2023-12-15 08:05:55 -0800

    Create test_process_molecule.py

---
+++ tests/test_process_molecule.py
@@ -0,0 +1,40 @@
+import unittest
+from chem_bio_utils_py.operations.process_molecules import processMolecule
+
+class TestProcessMolecule(unittest.TestCase):
+  def setUp(self):
+    smiles = "[H-]"
+    inchi = "InChI=1S/H/q-1"
+    
+  def test_valid_smiles(self):  # Test a valid SMILES string
+    result = processMolecule(self.smiles)
+    self.assertIsNotNone(result)
+    self.assertNotIsInstance(result, str)
+    self.assertEqual(self.inchi, result.inchi)
+
+  def test_invalid_smiles(self):  # Test an invalid SMILES string
+    invalid_smiles = "InvalidSMILES"
+    result = processMolecule(invalid_smiles)
+    self.assertIsInstance(result, str)
+    self.assertEqual(result, "Error processing input 'InvalidSMILES': Invalid input. Not SMILES or InChI.")
+
+  def test_valid_inchi(self):  # Test a valid InChI string
+    result = processMolecule(self.inchi)
+    self.assertIsNotNone(result)
+    self.assertNotIsInstance(result, str)
+    self.assertEqual(self.smiles, result.smiles)
+
+  def test_invalid_inchi(self):  # Test an invalid InChI string
+    invalid_inchi = "InvalidInChI"
+    result = processMolecule(invalid_inchi)
+    self.assertIsInstance(result, str)
+    self.assertEqual(result, "Error processing input 'InvalidInChI': Invalid input. Not SMILES or InChI.")
+
+  def test_empty_input(self):  # Test an empty string
+    empty_input = ""
+    result = processMolecule(empty_input)
+    self.assertIsInstance(result, str)
+    self.assertEqual(result, "Input is empty.")
+
+if __name__ == '__main__':
+  unittest.main()

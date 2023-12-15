41d6ff35e31e7e9799fc449fefc18c2f08f766c0
eniyakrishnaraj <78894087+eniyakrishnaraj@users.noreply.github.com>
2023-12-15 02:56:15 -0800

    Update crispr.py

--- chem_bio_utils_py/models/crispr.py
+++ chem_bio_utils_py/models/crispr.py
@@ -4,7 +4,6 @@ from dataclasses import dataclass
 class crispr:
   name: str
   pam_sequence: str  # Specify the PAM sequence of the CRISPR system
-  target_type: str  # Specify if the target is DNA or RNA
   spacer_length: int  # Spacer length for the system
   for_prefix: str  # Region necessary for Cas binding or reecognition
   for_suffix: str  # Region necessaary for increased stability

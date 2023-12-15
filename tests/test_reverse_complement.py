01f29830405cd8e7b057899068a6b0e9ea6418af
eniyakrishnaraj <78894087+eniyakrishnaraj@users.noreply.github.com>
2023-12-14 22:46:02 -0800

    Create test_reverse_complement.py

---
+++ tests/test_reverse_complement.py
@@ -0,0 +1,31 @@
+import unittest
+from chem_bio_utils_py.reverse_complement import ReverseComplement
+
+class TestReverseComplement(unittest.TestCase):
+  def setUp(self):
+    self.revcomp = ReverseComplement()
+
+  def test_transcribe(self):  # Tests a valid DNA seequence
+    dna_sequence = "ATGTCATTCGTAG"
+    expected_comp_sequence = "CTACGAATGACAT"
+    result = self.revcomp.reverseComp(dna_sequence)
+    self.assertEqual(result, expected_comp_sequence)
+
+  def test_lowercase_sequence(self):  # Test a DNA sequence with lowercase letters
+    lowercase_dna_sequence = "atgtcattcgtag"
+    expected_comp_sequence = "CTACGAATGACAT"
+    result_lowercase = self.revcomp.reverseComp(lowercase_dna_sequence)
+    self.assertEqual(result_lowercase, expected_comp_sequence)
+    
+  def test_empty_sequence(self):  # Test an empty DNA sequence
+    empty_dna_sequence = ""
+    result_empty =  self.revcomp.reverseComp(empty_dna_sequence)
+    self.assertEqual(result_empty, "DNA sequence is empty.")
+
+  def test_invalid_nucleotide(self):   # Tests a DNA sequence with invalid nucleotides (not 'A', 'T', 'C', or 'G')
+    invalid_nucleotide_sequence = "ATGTXATCGTAG"
+    result_invalid_nucleotide = self.revcomp.reverseComp(invalid_nucleotide_sequence)
+    self.assertEqual(result_invalid_nucleotide, "Invalid nucleotide found in DNA sequence.")
+
+if __name__ == "__main__":
+  unittest.main()

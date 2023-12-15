faa9c562712e3940cd70b29543fb2b38caf4bff3
eniyakrishnaraj <78894087+eniyakrishnaraj@users.noreply.github.com>
2023-12-14 18:28:13 -0800

    Initial commit

---
+++ tests/test_transcription.py
@@ -0,0 +1,31 @@
+import unittest
+from chem_bio_utils_py.transcription import DNATranscriber
+
+class TestDNATranscriber(unittest.TestCase):
+  def setUp(self):
+    self.transcriber = DNATranscriber()
+
+  def test_transcribe(self):  # Tests a valid DNA seequence
+    dna_sequence = "ATGTCATTCGTAG"
+    expected_rna_sequence = "UACAGUAGCUA"
+    result = self.transcriber.transcribe(dna_sequence)
+    self.assertEqual(result, expected_rna_sequence)
+
+  def test_lowercase_sequence(self):  # Test a DNA sequence with lowercase letters
+    lowercase_dna_sequence = "atgtcattcgtag"
+    expected_rna_sequence = "UACAGUAGCUA"
+    result_lowercase = self.transcriber.transcribe(lowercase_dna_sequence)
+    self.assertEqual(result_lowercase, expected_rna_sequence)
+    
+  def test_empty_sequence(self):  # Test an empty DNA sequence
+    empty_dna_sequence = ""
+    result_empty =  self.transcriber.transcribe(empty_dna_sequence)
+    self.assertEqual(result_empty, "DNA sequence is empty.")
+
+  def test_invalid_nucleotide(self):   # Tests a DNA sequence with invalid nucleotides (not 'A', 'T', 'C', or 'G')
+    invalid_nucleotide_sequence = "ATGTXATCGTAG"
+    result_invalid_nucleotide = self.transcriber.transcribe(invalid_nucleotide_sequence)
+    self.assertEqual(result_invalid_nucleotide, "Invalid nucleotide found in DNA sequence.")
+
+if __name__ = "__main__":
+  unittest.main()

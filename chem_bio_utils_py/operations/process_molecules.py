1a464c11428cf599830ef492a6ace658dd70f84c
eniyakrishnaraj <78894087+eniyakrishnaraj@users.noreply.github.com>
2023-12-15 08:42:59 -0800

    Update process_molecules.py

--- chem_bio_utils_py/operations/process_molecules.py
+++ chem_bio_utils_py/operations/process_molecules.py
@@ -15,13 +15,10 @@ def processMolecule(input):
     raise ValueError("Invalid input. Not valid SMILES or InChI.")
     
   # Processes the input and returns a molecule with all the attributes
-  try:
-    molecule = MoleculeModel(
-      inchi=Chem.MolToInchi(mol),
-      smiles=Chem.MolToSmiles(mol),
-      molecular_weight=Descriptors.MolWt(mol),
-      molecular_formula=Descriptors.MolecularFormula(mol),
-      fingerprint=Chem.RDKFingerprint(mol))
-    return molecule
-  except ValueError as e:
-    return f"Error processing input '{input}': {e}"
+  molecule = MoleculeModel(
+    inchi=Chem.MolToInchi(mol),
+    smiles=Chem.MolToSmiles(mol),
+    molecular_weight=Descriptors.MolWt(mol),
+    molecular_formula=Descriptors.MolecularFormula(mol),
+    fingerprint=Chem.RDKFingerprint(mol))
+  return molecule
